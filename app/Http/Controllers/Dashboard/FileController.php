c32282d4fabbdb26dba987da358a50807575c85d
Sunat Boev <sunat.boev@tcell.tj>
2023-02-05 20:38:04 +0500

    add 144p and 240p support

--- app/Http/Controllers/Dashboard/FileController.php
+++ app/Http/Controllers/Dashboard/FileController.php
@@ -23,6 +23,7 @@ class FileController extends Controller
             'path' =>  $path,
             'format' => $file->getClientOriginalExtension(),
             'quality' => $this->getVideoQuality($path),
+            'duration' => $this->getDuration($path),
         ]);
     }
 
@@ -40,6 +41,17 @@ class FileController extends Controller
         ]);
     }
 
+    private function getDuration($videoFile): string {
+        $ffmpeg = FFMpeg::create([
+            'ffmpeg.binaries' => '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/bin/ffprobe',
+        ]);
+        $video = $ffmpeg->open(storage_path('app/' . $videoFile));
+        $duration = $video->getStreams()->videos()->first()->get('duration');
+
+        return gmdate('H:i:s', $duration);
+    }
+
     private function getVideoQuality($videoFile): string {
         $ffmpeg = FFMpeg::create([
             'ffmpeg.binaries' => '/usr/bin/ffmpeg',
@@ -47,8 +59,25 @@ class FileController extends Controller
         ]);
         $video = $ffmpeg->open(storage_path('app/' . $videoFile));
         $videoStream = $video->getStreams()->videos()->first();
+        $width = $videoStream->get('width');
+        $height = $videoStream->get('height');
 
-        return $videoStream->get('bit_rate');
-
+        if ($width >= 3840 && $height >= 2160) {
+            return "4K";
+        } else if ($width >= 1920 && $height >= 1080) {
+            return "1080p";
+        } else if ($width >= 1280 && $height >= 720) {
+            return "720p";
+        } else if ($width >= 854 && $height >= 480) {
+            return "480p";
+        } else if ($width >= 640 && $height >= 360) {
+            return "360p";
+        } else if ($width >= 426 && $height >= 240) {
+            return "240p";
+        } else if ($width >= 256 && $height >= 144) {
+            return "144p";
+        } else {
+            return "Unknown";
+        }
     }
 }
