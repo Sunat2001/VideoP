38f398ec1f902d9567d4af1dc2c47202d7565297
Sunat Boev <sunat.boev@tcell.tj>
2023-02-07 21:35:11 +0500

    add console command for accumulate season rating and update README.md

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 ## About Project
 
-Project (after add description) using :
+Video Project using:
 
 - PHP 8.1
 - Laravel 9
 - Mysql 8
 - Docker
+
+## Installation
+For start project, you need to install docker and docker-compose, after that you can run this command :
+
+```bash
+docker-compose up -d
+```
+```bash
+docker exec myapp composer install
+```
+```bash
+docker exec myapp php artisan migrate --seed
+```
+```bash
+docker exec myapp php artisan key:generate
+```
+
+## Extend Artisan Commands
+```bash
+docker exec myapp php artisan season:reating
+```
 
 
 
---
+++ app/Console/Commands/AccumulateSeasonRating.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\SerialEpisodeSeason;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class AccumulateSeasonRating extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'season:rating';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command for accumulating season rating';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $this->info('Accumulating season rating...');
+        $seasons = SerialEpisodeSeason::all();
+        $bar = $this->output->createProgressBar(count($seasons));
+        foreach ($seasons as $season) {
+            $season->rating = $season->serialEpisodes()->avg('rating');
+            $season->save();
+            $bar->advance();
+        }
+        $bar->finish();
+        $this->info('Done!');
+        return CommandAlias::SUCCESS;
+    }
+}
