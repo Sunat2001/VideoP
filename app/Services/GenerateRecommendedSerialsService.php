1ea63002eb44b2a4f8f0d17ba0550e6961219723
Sunat Boev <sunat.boev@tcell.tj>
2023-02-14 21:33:22 +0500

    created functions for get most popular attribute values ids

--- app/Services/GenerateRecommendedSerialsService.php
+++ app/Services/GenerateRecommendedSerialsService.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Services;
 
+use App\Models\Serial;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 class GenerateRecommendedSerialsService
 {
@@ -10,8 +12,26 @@ class GenerateRecommendedSerialsService
     {
         $groupedSerials = $user->seenEpisodes()->with(['serial.attributeValues'])->get();
 
+        $attributeValuesIds = $this->calculateMostSeenAttributeValues($groupedSerials);
 
+        return $this->fetchSerialsByAttributeValues($attributeValuesIds);
+    }
 
-        return $groupedSerials;
+    private function fetchSerialsByAttributeValues(Collection $attributeValuesIds): Collection
+    {
+        $ids = $attributeValuesIds->sortDesc()->take(3)->keys()->toArray();
+
+        return Serial::with(['attributeValues'])->whereHas('attributeValues', function (Builder $query) use ($ids) {
+            $query->whereIn('id', $ids);
+        })->take(5)->get();
+    }
+
+    private function calculateMostSeenAttributeValues(Collection $groupedSerials): Collection
+    {
+        $result = $groupedSerials->map(function ($item) {
+            return $item->serial->attributeValues?->groupBy('attribute_id');
+        });
+
+        return $result->flatten()->countBy('id');
     }
 }
---
+++ database/seeders/RecomendedGenerateDataSeed.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RecomendedGenerateDataSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('user_episode_viewed')->insert([
+            'user_id' => 1,
+            'video_episode_id' => 1,
+        ]);
+
+        DB::table('user_episode_viewed')->insert([
+            'user_id' => 1,
+            'video_episode_id' => 2,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 1,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 25,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 26,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 67,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 68,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 4,
+            'attribute_value_id' => 69,
+        ]);
+
+        DB::table('serial_attribute_value')->insert([
+            'serial_id' => 14,
+            'attribute_value_id' => 69,
+        ]);
+
+    }
+}
